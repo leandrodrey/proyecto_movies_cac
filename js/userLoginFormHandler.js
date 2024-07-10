ea36d088662658498faeaf74824e45745984f69d
Leandro Rey <leandrodrey@gmail.com>
2024-07-10 19:51:38 -0300

    Update user login success response handling

--- js/userLoginFormHandler.js
+++ js/userLoginFormHandler.js
@@ -27,9 +27,35 @@ document.addEventListener("DOMContentLoaded", function () {
             });
 
             if (response.ok) {
+                const userData = await response.json();
                 alert("Inicio de sesión exitoso!");
                 console.log("Respuesta: ", response);
-                console.log("Usuario Logeado: ", response.json());
+                console.log("Usuario Logeado: ", userData);
+
+                const loginForm = document.querySelector(".loginForm");
+                loginForm.style.display = "none";
+                const loggedSection = document.querySelector(".logged");
+                loggedSection.appendChild(document.createTextNode("Bienvenido " + userData.user.name + "!!!"));
+                loggedSection.appendChild(document.createElement("br"));
+                loggedSection.appendChild(document.createElement("br"));
+                // Create the "Crear Película" button
+                const createButton = document.createElement('button');
+                createButton.textContent = 'Crear Película';
+                createButton.classList.add('boton');
+                createButton.addEventListener('click', () => {
+                    window.location.href = './crearpelicula.html';
+                });
+
+
+                const editButton = document.createElement('button');
+                editButton.textContent = 'Listado de Películas';
+                editButton.classList.add('boton');
+                editButton.addEventListener('click', () => {
+                    window.location.href = './listadodepeliculas.html';
+                });
+
+                loggedSection.appendChild(createButton);
+                loggedSection.appendChild(editButton);
             } else {
                 const errorData = await response.json();
                 alert("Error en el inicio de sesión: " + errorData.message || "Inténtalo nuevamente.");
